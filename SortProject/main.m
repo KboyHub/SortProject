56f593e5174401616dbadb94352d60a803522a2c
KboyHub <yankang_hpu@163.com>
2017-02-20 11:18:26 +0800

    修改一下

--- SortProject/main.m
+++ SortProject/main.m
@@ -8,22 +8,22 @@
 
 #import <Foundation/Foundation.h>
 #import "Sort.h"
-#define kSize 20
-#define kMax 100
+#define Size 20 //数组个数
+#define Max 100 //随机数范围（0-100）
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        NSMutableArray *data = [[NSMutableArray alloc] initWithCapacity:kSize];
+        NSMutableArray *data = [[NSMutableArray alloc] initWithCapacity:Size];
         
-        for (int i =0;i<kSize;i++) {
-            u_int32_t x = arc4random() % kMax;//0~kMax
+        for (int i =0;i<Size;i++) {
+            u_int32_t x = arc4random() % Max;//0~kMax
             NSNumber *num = [[NSNumber alloc] initWithInt:x];
             [data addObject:num];
         }
         NSLog(@"排序前的数据：%@",[data description]);
         Sort *sort = [[Sort alloc] init];
-//        [sort quickSortWithData:data leftIndex:0 rightIndex:data.count-1];／／快速排序
+        [sort quickSortWithData:data leftIndex:0 rightIndex:data.count-1];//快速排序
 //        NSLog(@"快速排序后的数据：%@",[data description]);
 //        [sort bunbleSortWithArray:data];//冒泡排序
 //        [sort selectSortWithArray:data];//选择排序
